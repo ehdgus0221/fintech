cad20fd3a35831c0b0fd7cc78f20427abb18595a
DONG <dlaehdgus23@naver.com>
2023-07-10 21:17:00 +0900

    file upload
    file upload

---
+++ api/src/main/kotlin/com/zerobase/api/test/TestDto.kt
@@ -0,0 +1,11 @@
+package com.zerobase.api.test
+
+
+class TestDto {
+    data class UserInfoDto(
+        val userKey: String,
+        val userRegistrationNumber: String,
+        val userName: String,
+        val userIncomeAmount: Long
+    )
+}
